38522be06621c5b3b2c5b9aa6c2d66e84ebda2b2
Smirkjuice <167714041+MaceWhite@users.noreply.github.com>
2024-08-06 08:40:03 +0000

    Update unbeatable_SPR.py

--- unbeatable_SPR.py
+++ unbeatable_SPR.py
@@ -1,3 +1,4 @@
+import os # for no reason
 print("Scissors, Paper, or Rock? - ")
 
 user_input = input()
@@ -8,5 +9,5 @@ elif (user_input == "Paper" or "paper"):
     print("Scissors, I win!")
 elif (user_input == "Rock" or "rock"): 
     print("Paper, I win!")
-elif (user_input != "Scissors" or "Paper" or "Rock"):
+elif (user_input != "Scissors" or "Paper" or "Rock" or "scissors" or "paper" or "rock"):
     print("That's not an option, so I still win!")
